89c00dec95444a56f1161d9733da54016b5b6a76
yannickeboo <85393914+yannickeboo@users.noreply.github.com>
2022-05-20 10:48:30 -0500

    Update jenkins_centos.sh

--- jenkins_centos.sh
+++ jenkins_centos.sh
@@ -1,4 +1,4 @@
-yum update -y
+sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
