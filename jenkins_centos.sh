239b2ae0d86e2fa7aff5ecadb18942f25f8db39f
yannickeboo <85393914+yannickeboo@users.noreply.github.com>
2022-05-20 11:00:53 -0500

    Update jenkins_centos.sh

--- jenkins_centos.sh
+++ jenkins_centos.sh
@@ -7,6 +7,9 @@ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade -y
 # Add required dependencies for the jenkins package
 sudo yum install java-11-openjdk -y
+sudo yum install git -y
+sudo yum install docker -y
+
 sudo yum install jenkins -y
 sudo systemctl daemon-reload
 sudo systemctl start jenkins
