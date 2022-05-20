b8ec6f2047b0614b54b83b47712adb844c36607f
yannickeboo <85393914+yannickeboo@users.noreply.github.com>
2022-05-20 10:44:42 -0500

    Create jenkins_centos.sh

---
+++ jenkins_centos.sh
@@ -0,0 +1,8 @@
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade -y
+# Add required dependencies for the jenkins package
+sudo yum install java-11-openjdk -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
